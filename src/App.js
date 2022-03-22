19a37144dc2a62e474858072754c5122d8d8f314
Pirada Boonna <73753271+earnpirada@users.noreply.github.com>
2022-03-22 17:33:13 +0700

    add bug

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React together
         </a>
       </header>
     </div>
